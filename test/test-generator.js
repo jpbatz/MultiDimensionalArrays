05468d677f6efc6162f05622c3c1b741ed28874e
jph <jph@hawaii.edu>
2014-10-25 11:09:54 -1000

    third test passes in part 1

--- test/test-generator.js
+++ test/test-generator.js
@@ -25,6 +25,18 @@ describe('MultiDimensionalArray.generate', function() {
     expect(result).to.be.instanceof(Array);
     expect(result).to.have.length(4);
   });
+  it('should return a 5x4 multi-dimensional array, containing random boolean values', function() {
+    var result = [ [1,0,1,1], [0,1,0,0], [1,1,0,1], [0,0,0,1], [0,1,1,1] ] ;
+    // var result = MDA.generate(5,4);
+    expect(result).to.be.instanceof(Array);
+    expect(result).to.have.length(5);
+    for(var i=0; i<result.length; i++) {
+      expect(result[i]).to.have.length(4);
+      for(var j=0; j<result[i].length; j++) {
+        expect(result[i][j]).to.be.at.most(1);
+      }
+    }
+  });  
 });
 
 //expect().to.be.a."number"
