6ca0996d2b455c0eeed6d47108d6357140f4a19b
jph <jph@hawaii.edu>
2014-10-27 12:41:54 -1000

    problem with buildLevel need to integrate with generate arguments not valid outside generate function

--- MultiDimensionalArrays.js
+++ MultiDimensionalArrays.js
@@ -1,32 +1,88 @@
 // SET UP AS A NODE MODULE ???
+module.exports = {
 
-function generate(array) {
+  generate: function() {
 
-  console.log(arguments.length);
-  if(arguments.length > 1) {
+    //console.log(arguments.length);
 
+    var result = [];
+    //var noArgs = false;
 
-  }
+    // 3 special cases:
+    if(arguments.length === 0) { // unspecified argument, assume 3x3
+        // call general case function
+        //noArgs = true;
+        for(var x=0; x<3; x++) {
+          result.push([]);
+          for(var y=0; y<3; y++) {
+            result[x].push(this.randomBinary());
+          }
+        }
+        //result = buildLevels();
+        return result;
+    } else if(arguments.length === 1) { // one dimensional array
+        for(var i=0; i<arguments[0]; i++) {
+          result = this.stuffArray(arguments[0]);
+        }
+    } else if(arguments.length === 2) { // two dimensional array
+        for(var j=0; j<arguments[0]; j++) {
+          result.push([]);
+          for(var k=0; k<arguments[1]; k++) {
+            result[j][k] = this.randomBinary();
+            //this.stuffArray(arguments[0]);
+          }
+        }
+    } else { // general case, more than 2 arguments
+        result = this.buildLevels();
+    }
   
-  return this; // return created array
-}
+    return result; // return created array
+  },
 
-function count() {
-  //return 3;
-}
+
+
+  count: function() {
+    //return count result;
+  },
 
 
 //generate(3,3);
 
 // random binary number generator produces 0 or 1
-function randomBinary() {
-  return Math.floor(Math.random() * 2);
-}
+  randomBinary : function() {
+    return Math.floor(Math.random() * 2);
+  },
 
-// console.log(randomBinary());
-// console.log(randomBinary());
-// console.log(randomBinary());
-// console.log(randomBinary());
-// console.log(randomBinary());
-// console.log(randomBinary());
-// console.log(randomBinary());
+  // returns an array populated with random binary values, as specified by numElements
+  stuffArray :function() { 
+    var stuffedArray = [];
+    if(arguments.length >= 1) {
+      numElements = arguments[arguments.length - 1];
+      for(var i=0; i<numElements; i++) {
+        stuffedArray.push(this.randomBinary());
+      }
+    }
+    return stuffedArray;
+  },
+
+  // take stuffedArray, and builds specified remaining levels 
+  buildLevels : function() {
+      var finalArray;
+      var level = arguments.length - 2;
+      // while(level) {
+      //   // buildLevel()
+
+      //   for(var i =0; i<=level; i++) {
+      //     finalArray = this.stuffArray();
+      //     finalArray[i].push(finalArray);
+      //   }
+
+      //   level--;
+      // }
+      //console.log(finalArray);
+    return finalArray;
+  }
+
+};
+
+
--- test/test-generator.js
+++ test/test-generator.js
@@ -10,38 +10,42 @@ var should = chai.should();
  
 describe('MultiDimensionalArray.generate', function() {
   it('should return an array of length 3, each is an array containing 3 random boolean values', function() {
-    //var result = MDA.generate();
-    var result = [ [0,1,0], [1,1,0], [0,1,1] ];
+    var result = MDA.generate();
+    //var result = [ [0,1,0], [1,1,0], [0,1,1] ];
     expect(result).to.be.instanceof(Array);
     expect(result).to.have.length(3);
     for(var i=0; i<result.length; i++) {
       expect(result[i]).to.have.length(3);
       for(var j=0; j<result[i].length; j++) {
+        // expect(result[i][j]).to.be.a.('number');
         expect(result[i][j]).to.be.at.most(1);
       }
     }
   });
   it('should return an array of length 4, containing random boolean values', function() {
-    var result = [1,0,1,1] ;
-    // var result = MDA.generate(4);
+    //var result = [1,0,1,1] ;
+    var result = MDA.generate(4);
     expect(result).to.be.instanceof(Array);
     expect(result).to.have.length(4);
   });
   it('should return a 5x4 multi-dimensional array, containing random boolean values', function() {
-    var result = [ [1,0,1,1], [0,1,0,0], [1,1,0,1], [0,0,0,1], [0,1,1,1] ] ;
-    // var result = MDA.generate(5,4);
+    //var result = [ [1,0,1,1], [0,1,0,0], [1,1,0,1], [0,0,0,1], [0,1,1,1] ] ;
+    var result = MDA.generate(5,4);
+    //console.log(result);
     expect(result).to.be.instanceof(Array);
     expect(result).to.have.length(5);
     for(var i=0; i<result.length; i++) {
       expect(result[i]).to.have.length(4);
       for(var j=0; j<result[i].length; j++) {
+        // expect(result[i][j]).to.be.a.('number');
         expect(result[i][j]).to.be.at.most(1);
       }
     }
   });
   it('should return a 3x4x5 multi-dimensional array, containing random boolean values', function() {
-    var result = [ [ [1,0,1,0,0], [0,1,0,0,1], [1,1,0,1,1], [0,0,0,0,1] ], [ [1,1,0,1,1], [1,0,1,0,0], [0,1,0,0,1], [0,0,0,0,1] ], [ [1,0,1,0,0], [1,1,0,1,1], [0,0,0,0,1], [0,1,0,0,1] ] ];
-    // var result = MDA.generate(5,4);
+    //var result = [ [ [1,0,1,0,0], [0,1,0,0,1], [1,1,0,1,1], [0,0,0,0,1] ], [ [1,1,0,1,1], [1,0,1,0,0], [0,1,0,0,1], [0,0,0,0,1] ], [ [1,0,1,0,0], [1,1,0,1,1], [0,0,0,0,1], [0,1,0,0,1] ] ];
+    var result = MDA.generate(3,4,5);
+    //console.log(result);
     expect(result).to.be.instanceof(Array);
     expect(result).to.have.length(3);
     for(var i=0; i<result.length; i++) {
@@ -49,6 +53,7 @@ describe('MultiDimensionalArray.generate', function() {
       for(var j=0; j<result[i].length; j++) {
         expect(result[i][j]).to.have.length(5);
         for(var k=0; k<result[i][j].length; k++) {
+          // expect(result[i][j][k]).to.be.a.('number');
           expect(result[i][j][k]).to.be.at.most(1);
         }
       }
@@ -57,4 +62,3 @@ describe('MultiDimensionalArray.generate', function() {
 });
 
 // remove commented var result lines
-//expect().to.be.a."number"
